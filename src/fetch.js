d02e987af9a38b9691a0ef6d3e9fe2d05fb7bd21
saranshgupta27 <43762009+saranshgupta27@users.noreply.github.com>
2021-03-22 18:31:03 +0530

    changes

---
+++ src/fetch.js
@@ -0,0 +1,20 @@
+import request from "./requests.js";
+const url = "https://api.themoviedb.org/3";
+let data_api = null;
+
+ function a(category) {
+ fetch(`${url}${request[category]}`, { method: "GET" })
+    // Converting received data to JSON
+    .then((response) => response.json())
+    .then((json) => set_data(json));
+
+  function set_data(json_data) {
+
+    data_api = json_data;
+    return json_data;
+  }
+
+  return data_api;
+}
+
+export default a;
---
+++ src/row.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function row() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default row
